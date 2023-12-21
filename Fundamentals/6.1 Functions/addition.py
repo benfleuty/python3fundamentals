5858085928cb15e4cfc7e4e7ed6d15d6592382cb
Ben Fleuty <bfleuty@outlook.com>
2023-12-21 13:23:37 +0000

    Add addition func

---
+++ Fundamentals/6.1 Functions/addition.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+
+num1 = float(input('Enter the first number:\n'))
+num2 = float(input('Enter the second number:\n'))
+
+result = add(num1,num2)
+print(f'The result is {result}')
